59e2272f71775905f30bcf10027cbb4fb020446f
Neurai <code@neurai.org>
2023-05-30 13:14:55 +0200

    Added explorer neuraiexplorer.com to neurai-qt. Thanks Satolight

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -122,7 +122,7 @@ static const int MAX_URI_LENGTH = 255;
 #define QAPP_APP_NAME_TESTNET "Neurai-Qt-testnet"
 
 /* Default third party browser urls */
-#define DEFAULT_THIRD_PARTY_BROWSERS "https://explorer.neurai.org/tx/%s"
+#define DEFAULT_THIRD_PARTY_BROWSERS "https://explorer.neurai.org/tx/%s|https://neuraiexplorer.com/tx/%s"
 
 /* Default IPFS viewer */
 #define DEFAULT_IPFS_VIEWER "https://ipfs.io/ipfs/%s"
