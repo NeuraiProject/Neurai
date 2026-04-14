f9d87b2819137b86f2c027f15d36a3d4005cbac8
NeuraiProject <code@neurai.org>
2026-04-14 10:02:27 +0200

    Remove obsolete configuration and test files from the repository.

--- confdefs.h
+++
@@ -1,8 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "Neurai"
-#define PACKAGE_TARNAME "neurai"
-#define PACKAGE_VERSION "1.0.6"
-#define PACKAGE_STRING "Neurai 1.0.6"
-#define PACKAGE_BUGREPORT "https://github.com/NeuraiProject/Neurai/issues"
-#define PACKAGE_URL "https://neurai.org/"
-#define HAVE_CXX17 1
