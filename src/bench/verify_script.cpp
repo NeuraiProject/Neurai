1339336fc09aaeac1de9a1c31f1be3d0541813f8
NeuraiProject <code@neurai.org>
2026-04-15 11:16:39 +0200

    fix: Ensure type safety by casting flags to unsigned int in VerifyScriptBench

--- src/bench/verify_script.cpp
+++ src/bench/verify_script.cpp
@@ -101,7 +101,7 @@ static void VerifyScriptBench(benchmark::State& state)
             txCredit.vout[0].scriptPubKey.data(),
             txCredit.vout[0].scriptPubKey.size(),
             txCredit.vout[0].nValue,
-            (const unsigned char*)stream.data(), stream.size(), 0, flags, nullptr);
+            (const unsigned char*)stream.data(), stream.size(), 0, static_cast<unsigned int>(flags.as_int()), nullptr);
         assert(csuccess == 1);
 #endif
     }
