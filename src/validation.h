21a127ed0e877361ef2586e6315c0da94f07d327
NeuraiProject <code@neurai.org>
2026-04-20 16:14:57 +0200

    fix: increase default ancestor and descendant limits for mempool transactions

--- src/validation.h
+++ src/validation.h
@@ -78,13 +78,13 @@ static const CAmount HIGH_TX_FEE_PER_KB = 0.1 * COIN;
 //! -maxtxfee will warn if called with a higher fee than this amount (in satoshis)
 static const CAmount HIGH_MAX_TX_FEE = 100 * HIGH_TX_FEE_PER_KB;
 /** Default for -limitancestorcount, max number of in-mempool ancestors */
-static const unsigned int DEFAULT_ANCESTOR_LIMIT = 200;
+static const unsigned int DEFAULT_ANCESTOR_LIMIT = 500;
 /** Default for -limitancestorsize, maximum kilobytes of tx + all in-mempool ancestors */
-static const unsigned int DEFAULT_ANCESTOR_SIZE_LIMIT = 250;
+static const unsigned int DEFAULT_ANCESTOR_SIZE_LIMIT = 750;
 /** Default for -limitdescendantcount, max number of in-mempool descendants */
-static const unsigned int DEFAULT_DESCENDANT_LIMIT = 200;
+static const unsigned int DEFAULT_DESCENDANT_LIMIT = 500;
 /** Default for -limitdescendantsize, maximum kilobytes of in-mempool descendants */
-static const unsigned int DEFAULT_DESCENDANT_SIZE_LIMIT = 250;
+static const unsigned int DEFAULT_DESCENDANT_SIZE_LIMIT = 750;
 /** Default for -mempoolexpiry, expiration time for mempool transactions in hours */
 static const unsigned int DEFAULT_MEMPOOL_EXPIRY = 336;
 /** Maximum kilobytes for transactions to store for processing during reorg */
